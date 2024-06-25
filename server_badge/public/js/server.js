cf81b051610f11ff6abc128aa40dc1ac5f92790a
husamhammad <husamhammad542@gmail.com>
2024-06-25 08:53:24 +0300

    feat:load the js file after the document created and be ready

--- server_badge/public/js/server.js
+++ server_badge/public/js/server.js
@@ -1,14 +1,16 @@
-frappe.call({
-    method: "server_badge.utils.get_server_type",
-    callback: function(response) {
-        const { server_type, badge_color } = response.message;
-        
-        if (server_type === "Stage") {
-            const badge = document.createElement('div');
-            badge.className = 'stage-server-badge';
-            badge.innerText = `${server_type}`;
+$(document).ready(function(){
+    frappe.call({
+        method: "server_badge.utils.get_server_type",
+        callback: function(response) {
+            const { server_type, badge_color } = response.message;
             
-            document.querySelector("header").prepend(badge);
+            if (server_type === "Stage") {
+                const badge = document.createElement('div');
+                badge.className = 'stage-server-badge';
+                badge.innerText = `${server_type}`;
+                
+                document.querySelector("header").prepend(badge);
+            }
         }
-    }
+    })
 })
