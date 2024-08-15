0f8988d40edde61e843795e14890b196d77283ba
husamhammad <husamhammad542@gmail.com>
2024-08-15 14:11:55 +0300

    feat:Convert main css file to be bundle to handling the ltr and rtl

--- server_badge/hooks.py
+++ server_badge/hooks.py
@@ -9,7 +9,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/server_badge/css/server.css"
+app_include_css = "server.bundle.css"
 app_include_js = "/assets/server_badge/js/server.js"
 
 # include js, css files in header of web template
